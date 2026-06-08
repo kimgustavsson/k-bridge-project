6138ba632ed962b40654f805ddc035875acbe6a2
kimdagus06 <dashagustavsson@gmail.com>
2026-06-08 12:49:00 +0200

    deleted a folder inside of k-bridge project
    deleted an identical folder inside of k-bridge project. testing if it's safe

--- k-bridge/k-bridge/src/lib/cn.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Conditionally joins class names together.
- * Lightweight alternative to clsx - filters out falsy values and joins with spaces.
- */
-export function cn(...classes: Array<string | false | null | undefined>): string {
-  return classes.filter(Boolean).join(' ');
-}
